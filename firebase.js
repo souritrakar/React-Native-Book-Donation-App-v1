81238394b983c19f04358bfd552fc87846a25f5a
Souritra Kar <64901415+souritrakar@users.noreply.github.com>
2021-03-25 22:14:56 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByHYkjS13YM9FHN-zJ9vf05jd00-_WZ5E",
-  authDomain: "booksanta-20159.firebaseapp.com",
-  projectId: "booksanta-20159",
-  storageBucket: "booksanta-20159.appspot.com",
-  messagingSenderId: "425406372365",
-  appId: "1:425406372365:web:a9ab100095d7fef4969344"
+  "YOUR_CREDENTIALS_HERE
   };
 
   firebase.initializeApp(firebaseConfig)
